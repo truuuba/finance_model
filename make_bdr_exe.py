29839d5693ed2986480801ff0a430216f9176ab2
truuuba <119066126+truuuba@users.noreply.github.com>
2024-09-02 16:57:44 +0300

    Update make_bdr_exe.py

--- make_bdr_exe.py
+++ make_bdr_exe.py
@@ -4,7 +4,7 @@ import pandas as pd
 from connect_db import sql
 from transliterate import translit
 
-def create_empty_excel(filename: str):
+def create_empty_excel(filename: str, columns, dt):
     df = pd.DataFrame(columns=columns, data=dt)
 
     if not os.path.exists('excel_files'):
@@ -23,11 +23,28 @@ def create_tabel_bdr(id_pr):
     n_t = del_probel_nazv(n_t)
     name_table = translit(n_t, language_code='ru', reversed=True)
 
-    #Не забыть удалить пробелы после месяцев
-    columns=make_first_list(mounth_w, dlit, year_w)
-    dt = count_bdr_rashodi(gpr, trati)
+    mount_w = del_probel_nazv(sql.take_mnth_st_w(id_pr))
+    mount_pr = del_probel_nazv(sql.take_mount_pr(id_pr))
+    dl = sql.take_dlit_project(id_pr)
+    year_w = sql.take_yr_st_w(id_pr)
+    year_prod = sql.take_year_prod(id_pr)
 
-    filepath = create_empty_excel(filename=('bdr_' + name_table + '.xlsx'))
+    columns = make_first_list(mnt_w=mount_w, mnt_prod=mount_pr, yr_w=year_w, yr_prod=year_prod, dlit=dl)
+    dannie_d = sql.take_stat(id_pr=id_pr, param="Доходы")
+    dannie_r = sql.take_stat(id_pr=id_pr, param="Расходы")
+
+    #Собираем данные для БДР
+    bdr_d = []
+    bdr_r = []
+    for el in dannie_d:
+        bdr_d.append(sql.take_data_bdr_dohodi(el.id_))
+    for el in dannie_r:
+        bdr_r.append(sql.take_data_bdr_rashodi(el.id_))
+
+    #Считаем БДР
+    dt = count_bdr_rashodi(columns, dannie_d, dannie_r, bdr_d, bdr_r)
+
+    filepath = create_empty_excel(filename=('bdr_' + name_table + '.xlsx'), columns=columns, dt=dt)
 
 def del_probel_nazv(nm):
     n = len(nm)
@@ -37,10 +54,8 @@ def del_probel_nazv(nm):
         else:
             return nm
         
-def make_first_list(mnt_w, mnt_prod, yr_w, yr_prod):
-    arr_time = []
-    mnt = ''
-    yr = 0
+def make_first_list(mnt_w, mnt_prod, yr_w, yr_prod, dlit):
+    arr_time = [""]
 
     #Проверка на то, что раньше
     prov_w = False
@@ -58,9 +73,112 @@ def make_first_list(mnt_w, mnt_prod, yr_w, yr_prod):
         else:
             #Проверка строительства
             prov_w = True
+    
+    #Условие для продаж перед строительством
+    if prov_prod:
+        prov = mnt_w + " " + str(yr_w)
+        tmp = mnt_prod + " " + str(yr_prod)
+        mnt = mnt_prod
+        yr = yr_prod
+        while (prov != tmp):
+            arr_time.append(tmp)
+            mnt = change_mnt(mnt)
+            if mnt == 'Январь':
+                yr += 1
+            tmp = mnt + " " + yr
+        for i in range(dlit):
+            arr_time.append(mnt + " " + yr)
+            mnt = change_mnt(mnt)
+            if mnt == 'Январь':
+                yr += 1
+            tmp = mnt + " " + yr
+    
+    #Условие для строительства перед продажами
+    if prov_w:
+        prov = mnt_prod + " " + str(yr_prod)
+        tmp = mnt_w + " " + str(yr_w)
+        mnt = mnt_w
+        yr = yr_w
+        while (prov != tmp):
+            arr_time.append(tmp)
+            mnt = change_mnt(mnt)
+            if mnt == 'Январь':
+                yr += 1
+            tmp = mnt + " " + yr
+        for i in range(dlit):
+            arr_time.append(mnt + " " + yr)
+            mnt = change_mnt(mnt)
+            if mnt == 'Январь':
+                yr += 1
+            tmp = mnt + " " + yr
+    
+    return arr_time
+
+#Меняем месяца
+def change_mnt(mounth):
+    arr = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
+    ind = 0
+    for i in range(len(arr)):
+        if arr[i] == mounth:
+            ind = i
+            break
+    if ind == 11:
+        return arr[0]
+    else:
+        return arr[ind+1]
 
 def found_ind_mnt(mnt):
     arr = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
     for i in range(len(arr)):
         if arr[i] == mnt:
             return i
+
+#считаем БДР
+def count_bdr_rashodi(columns, d_d, d_r, bdr_d, bdr_r):
+    table_gpr = []
+
+    #идем по каждой статье доходов
+    for el in d_d:
+        temp = [] #массив под бдр
+        temp.append(el.nazv)
+        mas_el = [] #массив под статью дохода
+
+        #вытаскиваем все записи по этой статье
+        for elem in bdr_d:
+            if elem.id_st == el.id_:
+                mas_el.append(elem)
+
+        #проверяем наличие доходов в этом месяце
+        for i in range(len(columns)):
+            for j in range(len(mas_el)):
+                tp = mas_el[i].mnt + " " + mas_el[j].yr
+                if columns[i] == tp:
+                    temp.append(mas_el.dohodi)
+                else:
+                    temp.append(0)
+        
+        table_gpr.append(temp)
+    
+    #идем по каждой статье расходов
+    for el in d_r:
+        temp = [] #массив под бдр
+        temp.append(el.nazv)
+        mas_el = [] #массив под статью расходов
+
+        #вытаскиваем все записи по этой статье
+        for elem in bdr_r:
+            if elem.id_st == el.id_:
+                mas_el.append(elem)
+
+        #проверяем наличие расходов в этом месяце
+        for i in range(len(columns)):
+            for j in range(len(mas_el)):
+                tp = mas_el[i].mnt + " " + mas_el[j].yr
+                if columns[i] == tp:
+                    temp.append(mas_el.trati)
+                else:
+                    temp.append(0)
+        
+        table_gpr.append(temp)
+    
+    return table_gpr
